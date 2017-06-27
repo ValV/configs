596e9faf68055ae3b28533e13ca79bd68277f988
Vladimir Valeyev <ValV@users.noreply.github.com>
2017-06-27 12:26:35 +0600

    Add Mod4+Up (Super+UpArrow) and Mod4+Down (Super+DownArrow) client cycle by index key bindings

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -253,7 +253,19 @@ globalkeys = gears.table.join(
         end,
         {description = "focus next by index", group = "client"}
     ),
+    awful.key({ modkey,           }, "Up",
+        function ()
+            awful.client.focus.byidx( 1)
+        end,
+        {description = "focus next by index", group = "client"}
+    ),
     awful.key({ modkey,           }, "k",
+        function ()
+            awful.client.focus.byidx(-1)
+        end,
+        {description = "focus previous by index", group = "client"}
+    ),
+    awful.key({ modkey,           }, "Down",
         function ()
             awful.client.focus.byidx(-1)
         end,
